6d3a0d4767738c3c8b4575eb74eca6ca1442672a
unknown <kei@4101112-PC.morijyobi.intra>
2012-06-13 12:11:08 +0900

    Listviewのレイアウトを変更しました（仮）

---
+++ jisyooo0003/src/com/android/jisyooo/A_gyou.java
@@ -0,0 +1,85 @@
+package com.android.jisyooo;
+
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import android.view.View;
+import android.widget.AdapterView;
+
+
+public class A_gyou extends Activity{
+
+	ListView lv = null;
+
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.a_gyou);
+        
+        
+      //ListViewを取得
+        this.lv = (ListView)this.findViewById(R.id.ki_list);
+        
+        //項目の配列を準備
+        String[] item = {"おはよがんす", "おばんでがんす", "ごめんくなんしぇ", "おやすみんしぇ" ,
+				  "もっし","お静かに","ご油断なく","ゆきちくれ"};
+         
+       /* String[] imi = {"おはよう", "こんばんは", "ごめんください", "おやすみなさい" ,
+  			  "ごめんください","さようなら","お気をつけてお帰りくださいませ","金よこせ"}; */
+        
+        
+        //項目のオブジェクトを準備
+//        ArrayAdapter ad = new ArrayAdapter(this, android.R.layout.simple_list_item_1, item);
+        
+        //
+           ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+              this,
+              R.layout.list_main,
+              R.id.row_textview,
+              item);
+          
+        //リストビューに項目オブジェクトをセット
+        lv.setAdapter(adapter);
+        
+        //リストビュークリック時のイベントを追加
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        	
+        @Override
+        public void onItemClick(AdapterView arg0, View arg1, int arg2,long arg3) {
+        //呼び出したListViewを取得
+        ListView listView = (ListView)arg0;
+        
+        //クリックした項目を取得
+        String item = (String)listView.getItemAtPosition(arg2);
+ 
+        
+     // 選択されたデータをダイアログで表示
+        AlertDialog.Builder dialog = new AlertDialog.Builder(A_gyou.this);
+        dialog.setTitle(item); //タイトル
+        
+        dialog.setMessage("意味：" );
+       
+        
+        dialog.setCancelable(false)
+        .setPositiveButton("close", new DialogInterface.OnClickListener() {
+			
+			@Override
+			public void onClick(DialogInterface dialog, int id) {
+				// TODO 自動生成されたメソッド・スタブ
+				dialog.cancel(); //ダイアログを閉じる
+				
+			}
+		});
+
+	dialog.show();
+        }
+        });
+       
+	}
+	
+}
